901e243f46ec54b1d6b9372528aa4e70fe719ac1
Sherlin-2002 <sherlindaison2020@gmail.com>
2024-06-15 12:15:35 +0530

    first commit

---
+++ nestedloop.py
@@ -0,0 +1,9 @@
+row= int(input("Enter the row:"))
+column= int(input("Enter the no of column:"))
+symbol= input("Enter the symbol")
+for i in range(row):
+    for j in range(column):
+        print(symbol , end="")
+
+    print()
+  
